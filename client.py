505c7b76b62135dc2c3221c5614928ad51a8dfe7
Alberto Ferreira <AlbertoEAF@users.noreply.github.com>
2021-04-03 22:18:59 +0100

    update with setups

--- client.py
+++ client.py
@@ -1,23 +1,30 @@
 import argparse
 import subprocess
+import time
 
 def start_jacktrip_client(server_ip):
     print("Starting jacktrip client connection...")
-    subprocess.check_call(f".\jacktrip.exe -C {server_ip} -v")
+    subprocess.check_call(f".\jacktrip.exe -C {server_ip} -V")
     print("jacktrip Session is over.")
 
 def main():
-    print("### Launching Jacktrip client ###")
     parser = argparse.ArgumentParser()
     parser.add_argument("server_ip", help="JackTrip Server IP")
     parser.add_argument("--retry", action="store_true", help="Try to start a new session if this one fails.")
     args = parser.parse_args()
 
+    print("### Launching Jacktrip client ###")
+
     while True:
         start_jacktrip_client(args.server_ip)
         if not args.retry:
             break
+        else:
+            print("\n\nSession ended. Starting a new one...\n\n")
+            time.sleep(2)
 
 
 
 
+if __name__ == "__main__":
+    main()
